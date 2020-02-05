36cd458182d3e3b020381a76578c0014ca46c486
kbens <kyle@psadmin.io>
2020-02-05 11:10:09 -0600

    added start and stop

--- psadmin_plus/actions/start.py
+++ psadmin_plus/actions/start.py
@@ -1,16 +1,16 @@
 from psadmin_plus.actions.action import Action
-
+        
 class Start(Action):
 
     def __init__(self):
         super().__init__()
 
     def _app(self,domain):
-        print('start app ' + domain)
-        print(self.conf.test) 
+        self._psadmin(["-c","boot","-d",domain])
+        # TODO - parallel switch - self._psadmin(["-c","parallelboot","-d",domain])
 
     def _prcs(self,domain):
-        print('start prcs ' + domain)
+        self._psadmin(["-p","status","-d",domain])
 
     def _web(self,domain):
-        print('start web ' + domain)
+        self._psadmin(["-w","status","-d",domain])

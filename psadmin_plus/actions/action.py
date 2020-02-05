2eee3c0c691a9768c9c58c9136ae2539345fe2e2
kbens <kyle@psadmin.io>
2020-02-05 16:44:18 +0000

    moved psadmin call to Action

--- psadmin_plus/actions/action.py
+++ psadmin_plus/actions/action.py
@@ -1,11 +1,15 @@
+import subprocess
+
 from psadmin_plus.interface.conf import Conf
+
+# TODO - we are assuming linux, run as psadm2, psadmin in patch, etc 
 
 class Action:
 
     def __init__(self):
         self.conf = Conf()
 
-    def process(self,type,domain):
+    def process(self, type, domain):
         if type == 'app':
             self._app(domain)
         elif type == 'prcs':
@@ -15,11 +19,14 @@ class Action:
         else:
             raise ValueError('Invalid domain type provided to Action')
 
-    def _app(self,domain):
+    def _app(self, domain):
         print('action app ' + domain)
 
-    def _prcs(self,domain):
+    def _prcs(self, domain):
         print('action prcs ' + domain)
 
-    def _web(self,domain):
+    def _web(self, domain):
         print('action web ' + domain)
+
+    def _psadmin(self, args):
+        subprocess.call(["psadmin"] + args)
--- psadmin_plus/actions/status.py
+++ psadmin_plus/actions/status.py
@@ -1,24 +1,18 @@
-import subprocess # TODO move to Action
-
 from psadmin_plus.actions.action import Action
         
-# TODO - right now assuming run as psadm2 and all ENVs are set, psadmin in path, etc
-# TODO - add a psadmin command method in Action, that takes args
-# TODO - use subprocessPopen to better handle output, poll progress, etc
-
 class Status(Action):
 
     def __init__(self):
         super().__init__()
 
     def _app(self,domain):
-        subprocess.call(["psadmin", "-c","sstatus","-d",domain])
-        subprocess.call(["psadmin", "-c","cstatus","-d",domain])
-        subprocess.call(["psadmin", "-c","qstatus","-d",domain])
-        subprocess.call(["psadmin", "-c","pslist","-d",domain])
+        self._psadmin(["-c","sstatus","-d",domain])
+        self._psadmin(["-c","cstatus","-d",domain])
+        self._psadmin(["-c","qstatus","-d",domain])
+        self._psadmin(["-c","pslist","-d",domain])
 
     def _prcs(self,domain):
-        subprocess.call(["psadmin", "-p","status","-d",domain])
+        self._psadmin(["-p","status","-d",domain])
 
     def _web(self,domain):
-        subprocess.call(["psadmin", "-w","status","-d",domain])
+        self._psadmin(["-w","status","-d",domain])

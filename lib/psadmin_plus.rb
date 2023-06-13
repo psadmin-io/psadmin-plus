8d81348503333c8e1e2363fccb588db414fc9bd5
Dan Iverson <dan@daniverson.com>
2023-06-13 11:15:02 -0500

    Capture only first line

--- lib/psadmin_plus.rb
+++ lib/psadmin_plus.rb
@@ -303,7 +303,7 @@ def do_status(type, domain, tuxcmd)
             # output = do_cmd("echo #{cmd} | " + env('TUXDIR') + "/bin/tmadmin -r ") #| grep PS |  while IFS= read -r line; do printf '[%s] %s\n' \"\$\(date '+%Y-%m-%d %H:%M:%S'\)\" \"\$line\"; done")
             output = do_cmd("export TUXCONFIG=#{env('PS_CFG_HOME')}/appserv/#{domain}/PSTUXCFG && echo #{cmd} | " + env('TUXDIR') + "/bin/tmadmin -r ")
             # output.split ('\n').each do | line |
-            copyright, rights, license, trademark, *lines = output.split('\n')
+            *lines = output.split('\n')
             p "Lines: " + lines.to_s
             lines.each do | line |
                 p Time.now.strftime("[%Y-%m-%d %H:%M:%S] ") + line

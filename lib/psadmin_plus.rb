3d8a12ccd0897900c95812fa4eb68d3b170eb9aa
Dan Iverson <dan@daniverson.com>
2023-06-13 11:37:49 -0500

    Correctly splitting new lines

--- lib/psadmin_plus.rb
+++ lib/psadmin_plus.rb
@@ -303,9 +303,8 @@ def do_status(type, domain, tuxcmd)
             # output = do_cmd("echo #{cmd} | " + env('TUXDIR') + "/bin/tmadmin -r ") #| grep PS |  while IFS= read -r line; do printf '[%s] %s\n' \"\$\(date '+%Y-%m-%d %H:%M:%S'\)\" \"\$line\"; done")
             output = do_cmd("export TUXCONFIG=#{env('PS_CFG_HOME')}/appserv/#{domain}/PSTUXCFG && echo #{cmd} | " + env('TUXDIR') + "/bin/tmadmin -r ")
             # output.split ('\n').each do | line |
-            *lines = output.split('\n')
+            *lines = output.split(/\n/)
             p "Lines: " + lines.to_s
-            p "Lines object " + lines.type
             lines.each do | line |
                 p Time.now.strftime("[%Y-%m-%d %H:%M:%S] ") + line
             end

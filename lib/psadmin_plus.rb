d6c01d691fded2c35c7ab120f93f600d6c77138c
Dan Iverson <dan@daniverson.com>
2023-06-14 11:31:33 -0500

    Use method call

--- lib/psadmin_plus.rb
+++ lib/psadmin_plus.rb
@@ -63,8 +63,8 @@ end
 def colorize(text, color_code); "\e[#{color_code}m#{text}\e[0m"; end
 def red(text); colorize(text, 31); end
 def green(text); colorize(text, 32); end
-def info(msg); logger.info msg; end
-def debug(msg); logger.debug msg; end
+def info(msg); logger.info(msg); end
+def debug(msg); logger.debug(msg); end
 
 def do_is_runtime_user_nix
     result = ENV['USER'] == PS_RUNTIME_USER ? true : false

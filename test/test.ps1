b140527f5d8aaaba8cea0c7de25cfd10f6f2a685
Dan Iverson <dan@daniverson.com>
2023-06-16 10:22:51 -0500

    Add env var tests

--- test/test.ps1
+++ test/test.ps1
@@ -7,9 +7,24 @@ $commands = @(
   "list"
 )
 
+# Build the array of environment variables to set
+$variables = @(
+  "PS_PSA_OUTPUT=all",
+  "PS_PSA_OUTPUT=summary",
+  "PS_PSA_TIMESTAMP=true",
+  "PS_PSA_DEBUG=DEBUG"
+)
+
 # Loop through the array and execute each command
 foreach ($cmd in $commands) {
   Write-Host "Executing command: $cmd"
+
+  # Set the environment variables for the current command
+  foreach ($var in $variables) {
+    $envVar = $var -split "="
+    Set-Item -Path "env:$($envVar[0])" -Value $envVar[1]
+  }
+
   Invoke-Expression "bin/psa $cmd"
 
   # Verify the result of the command
--- test/test.sh
+++ test/test.sh
@@ -9,10 +9,25 @@ commands=(
   "list"
 )
 
+# Build the array of environment variables to set
+variables=(
+  "PS_PSA_OUTPUT=all"
+  "PS_PSA_OUTPUT=summary"
+  "PS_PSA_TIMESTAMP=true"
+  "PS_PSA_DEBUG=DEBUG"
+)
+
 # Loop through the array and execute each command
 for cmd in "${commands[@]}"
 do
   echo "Executing command: $cmd"
+
+  # Set the environment variables for the current command
+  for var in "${variables[@]}"
+  do
+    export "$var"
+  done
+
   eval "bin/psa $cmd"
 
   # Verify the result of the command
